b066639c0d754035c10211017b9f0d3222afc27f
Madic- <3735459+Madic-@users.noreply.github.com>
2019-02-08 01:21:49 +0100

    Further corrections for the manual test script

--- tests/manual_test.sh
+++ tests/manual_test.sh
@@ -22,17 +22,12 @@ docker exec --tty "$CID" env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/ro
 docker exec --tty "$CID" env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/role_under_test/tests/ansible.cfg ansible-playbook /etc/ansible/roles/role_under_test/tests/playbook.yml
 
 # Run the role/playbook again, checking to make sure it's idempotent.
-#idempotence="$CID"
-#'sudo docker exec "$CID"  env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/role_under_test/tests/ansible.cfg ansible-playbook \
-# /etc/ansible/roles/role_under_test/tests/playbook.yml --extra-vars "make_migration=false" | tee -a ${idempotence}'
-#>
-#tail ${idempotence}
-#| grep -q 'changed=0.*failed=0'
-#&& (echo 'Idempotence test: pass' && exit 0)
-#|| (echo 'Idempotence test: fail' && exit 1)
+#docker exec "$CID" env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/role_under_test/tests/ansible.cfg ansible-playbook \
+#/etc/ansible/roles/role_under_test/tests/playbook.yml --extra-vars "make_migration=false" | tee -a idempotence \
+#tail idempotence | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
 
 # Test role.
-#'sudo docker exec --tty "$CID" env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/role_under_test/tests/ansible.cfg ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml'
+#docker exec --tty "$CID" env TERM=xterm env ANSIBLE_CONFIG=/etc/ansible/roles/role_under_test/tests/ansible.cfg ansible-playbook /etc/ansible/roles/role_under_test/tests/test.yml
 
 docker stop "$CID"
 
